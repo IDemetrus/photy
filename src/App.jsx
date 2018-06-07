94a7062aa70a047dd748192be5baedc9080bc551
D.I <workhome.pro@gmail.com>
2018-06-07 21:25:57 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { connect } from 'react-redux'
+
+
+
+class App extends Component {
+  render() {
+    console.log(this.props)
+    let btn;
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Photo App</h1>
+        </header>
+        <p className="App-intro">
+          Welcome, {this.props.name}!
+          <button className='App-btn' ref={btn}
+            
+            onClick={this.props.loginR}>
+            Sign in
+          </button>
+        </p>
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    name: state.name
+  }
+}
+const mapDispatchToProps = (dispatch) => {
+  return {
+    //loginR = login request*
+    loginR: () => dispatch({type: 'LOGIN_REQUEST'})
+  }
+}
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);//this method connect one function(getState) with second(dispatch) to this component App/
---
+++ src/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App.jsx';
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+
+// --------------------------------
+const userState = { name: 'Anonymous'} // init state (for user in this case)
+const reducer = (state = userState, action) => {
+    if(action.type === 'LOGIN_REQUEST'){
+        console.log(action)
+        return state = { name: 'User'}
+    }else{
+        return state
+    }
+}
+const store = createStore(reducer) // get changes from rootReducer
+window.store = store // this command for console view of store
+
+store.subscribe(() => {
+    console.log('store has been changed')
+})
+// --------------------------------
+
+ReactDOM.render(
+    /* 1. set Provider for connecting react with redux */
+    /* 2. set our store as props of Provider  */
+    <Provider store={store}> 
+        <App />
+    </Provider>,
+    document.getElementById('root'));
