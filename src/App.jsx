2d66d28379137a4e041854394f48167750f06cc1
D.I <workhome.pro@gmail.com>
2018-06-08 17:30:19 +0300

    run 1st

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,6 @@ class App extends Component {
         </header>
         <p className="App-intro">
           Welcome, {this.props.name}!
-          Albums
           <button className='App-btn' ref={btn}
             onClick={this.props.loginR}>
             Sign in
@@ -34,11 +33,35 @@ const mapStateToProps = (state) => {
   }
 }
 const mapDispatchToProps = (dispatch) => {
-  return {
+
     //loginR = login request*
-    loginR: () => dispatch({type: 'LOGIN_REQUEST'})
+    //loginR: () => dispatch({type: 'LOGIN_REQUEST'})
+    return function loginR(dispatch) {
+
+      dispatch({
+        type: 'LOGIN_REQUEST'
+      })
+  
+      VK.Auth.login((r) => { // eslint-disable-line no-undef
+        if (r.session) {
+          let username = r.session.user.first_name;
+          dispatch({
+            type: 'LOGIN_SUCCES',
+            payload: username
+          })
+  
+        } else {
+          dispatch({
+            type: 'LOGIN_FAIL',
+            error: true,
+            payload: new Error('Ошибка авторизации')
+          })
+        }
+      },4); // запрос прав на доступ к photo
+    }
+
   }
-}
+
 
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);//this method connect one function(getState) with second(dispatch) to this component App/
